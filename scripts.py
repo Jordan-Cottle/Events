f657b78552a3ca1462d865459db1ae46b716dc87
Jordan Cottle <jordancottle622@gmail.com>
2021-08-22 01:45:09 -0400

    Add publish script

--- scripts.py
+++ scripts.py
@@ -1,13 +1,19 @@
 #! /usr/bin/env python
 """ Scripts for development """
 
+import json
 import os
-from typing import Iterable
-from subprocess import run
-
 from argparse import ArgumentParser
+from http import HTTPStatus
+from subprocess import PIPE, CalledProcessError, run
+from typing import Callable, Iterable, Mapping
+from urllib.error import HTTPError
+from urllib.parse import urlparse
+from urllib.request import urlopen
 
 SOURCE_CODE_LOCATIONS = " ".join(["py_events", "tests", __file__])
+PYPI = os.getenv("PYPI", "pypi")
+PROJECT_NAME = "py-events"
 
 assert (
     os.getenv("VIRTUAL_ENV") is not None
@@ -16,7 +22,15 @@ assert (
 
 def _run(command):
     print(command)
-    return run(command, check=True, shell=True)
+
+    try:
+        output = run(command, check=True, shell=True, text=True, stdout=PIPE).stdout
+    except CalledProcessError as error:
+        print(error.stdout)
+        raise
+
+    print(output)
+    return output.strip()
 
 
 def run_black(options: Iterable[str] = ("--check", "--diff")) -> None:
@@ -56,12 +70,72 @@ def test(options: Iterable[str] = ()) -> None:
     _run(f"pytest {' '.join(options)}")
 
 
-COMMANDS = {
+def check_version() -> None:
+    """Assert that the latest git tag matches the poetry version."""
+
+    current_version = _run("poetry version -s")
+    print(f"Latest version: {current_version}")
+
+    if "GITHUB_ACTIONS" in os.environ:
+        # This enables the git describe command below to work since by default the information
+        # required by `git describe` is not checkout out within the actions environment
+        _run("git fetch --prune --unshallow")
+
+    git_tag = _run("git describe --abbrev=0")
+    if git_tag != f"v{current_version}":
+        raise ValueError(
+            f"Git tag ({git_tag}) does not match poetry version ({current_version})"
+        )
+
+
+def publish() -> None:
+    """Publish to pypi if there is a new version.
+
+    Authentication for publishing needs to be configured beforehand.
+    """
+
+    check_version()
+
+    scheme, net_location, *_ = urlparse(_run(f"poetry config repositories.{PYPI}.url"))
+
+    pypi_host = f"{scheme}://{net_location}"
+
+    try:
+        with urlopen(f"{pypi_host}/pypi/{PROJECT_NAME}/json") as response:
+            meta_data = json.loads(response.read())
+            latest_published_version = meta_data["info"]["version"].strip()
+    except HTTPError as error:
+        if error.code == HTTPStatus.NOT_FOUND:
+            latest_published_version = None
+        else:
+            raise
+
+    print(
+        f"Latest version of {PROJECT_NAME} on {pypi_host}: '{latest_published_version}'"
+    )
+
+    current_version = _run("poetry version -s")
+    print(f"Current version of {PROJECT_NAME} is '{current_version}'")
+
+    if current_version == latest_published_version:
+        print(f"{pypi_host} has the most recent version, no publish required")
+        return
+
+    print(f"Newer version of {PROJECT_NAME} detected.")
+    if PYPI != "pypi":
+        _run(f"poetry publish -r {PYPI}")
+    else:
+        _run("poetry publish")
+
+
+COMMANDS: Mapping[str, Callable] = {
     "check": check,
     "black": run_black,
     "pylint": run_pylint,
     "mypy": run_mypy,
     "test": test,
+    "check_version": check_version,
+    "publish": publish,
 }
 
 if __name__ == "__main__":
